4bc6665c37ad75510467c834bec92b6368066aa6
babyzombievarkenmankipbereider <94361888+zombievarkenman@users.noreply.github.com>
2022-02-22 13:02:41 +0100

    homepage bijna af en berekeningen bijna af

--- js/algemeen.js
+++ js/algemeen.js
@@ -1,10 +1,33 @@
-function RestWaarden(){
-    let basisgetal = parseInt(document.getElementById("RestwaardenBasisGetal").value);
-    let extragetal = parseInt(document.getElementById("RestwaardenExtraGetal").value);
-
+function showEvenOfOneven(){
+    alert(asdf);
+    let antwoordEvenOfOneven = evenOfOnevenBerekening();
+    document.getElementById("antwoordEvenOfOneven").innerHTML = antwoordEvenOfOneven;
 }
 
-function EvenOfOneven(){
-    let EvenOfOneven = parseInt(document.getElementById("EvenOfOneven").value);
-    
+function evenOfOnevenBerekening(){
+    let evenOfOnevenvalue = document.getElementById("EvenOfOneven").value;
+    let evenOfOneven = even % 10;
+    let evenNummers = [0,2,4,6,8];
+    let even = "dit getal is even";
+    let oneven = "dit getal is oneven";
+    if(evenOfOneven == evenNummers){
+        return even;
+    } else {
+        return oneven;
+    }
 }
+
+document.getElementById("antwoordEvenOfOnevenButton").addEventListener("click",showEvenOfOneven);
+
+function showRestwaarden(){
+    let antwoordRestwaarden = restwaardenBerekening();
+    document.getElementById("antwoordRestwaarden").innerHTML = antwoordRestwaarden;
+}
+
+function restwaardenBerekening(){
+    basisGetal = document.getElementById("RestwaardenBasisGetal").value;
+    extraGetal = document.getElementById("RestwaardenExtraGetal").value;
+    while(basisGetal !=0)
+}
+
+// De gebruiker kan 2 getallen invoeren, het basis getalen het getal wat er iedere keer van af gehaald wordt, en als hij op de knop “Rest” klikt, dan moet derestwaardehiervan getoond worden.
