0cb863794e8efd4305d8d9e275f678314619405d
babyzombievarkenmankipbereider <94361888+zombievarkenman@users.noreply.github.com>
2022-02-22 18:31:33 +0100

    kleine extra voor berekeningen

--- js/algemeen.js
+++ js/algemeen.js
@@ -27,7 +27,10 @@ function showRestwaarden(){
 function restwaardenBerekening(){
     basisGetal = document.getElementById("RestwaardenBasisGetal").value;
     extraGetal = document.getElementById("RestwaardenExtraGetal").value;
-    while(basisGetal !=0)
+    while(basisGetal >=0){
+        basisGetal - extraGetal
+    }
 }
 
+document.getElementById("antwoordRestwaardenButton").addEventListener("click",showRestwaarden);
 // De gebruiker kan 2 getallen invoeren, het basis getalen het getal wat er iedere keer van af gehaald wordt, en als hij op de knop “Rest” klikt, dan moet derestwaardehiervan getoond worden.
