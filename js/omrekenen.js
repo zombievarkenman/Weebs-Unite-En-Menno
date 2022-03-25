84bcc70caeb846eabe7715149233493b63d8d545
babyzombievarkenmankipbereider <94361888+zombievarkenman@users.noreply.github.com>
2022-03-25 15:59:11 +0100

    begin aan romanizer

--- js/omrekenen.js
+++ js/omrekenen.js
@@ -1,14 +1,15 @@
 function showromanizer(){
-    let een = parseInt(document.getElementById("romanizer").value) % 10;
-    let tien = (parseInt(document.getElementById("romanizer").value) / 10) % 10;
-    let honderd = (parseInt(document.getElementById("romanizer").value) / 100) % 10;
-    let duizend = (parseInt(document.getElementById("romanizer").value) / 1000) % 10;
-    antwoord = berekeningromanizer(een, tien, honderd, duizend);
+    document.getElementById("antwoordromanizer").innerHTML = "";
+    let getal = parseInt(document.getElementById("romanizer").value);
+    antwoord = berekeningromanizer(getal);
     document.getElementById("antwoordromanizer").innerHTML = antwoord;
 }
+document.getElementById("antwoordromanizerbutton").addEventListener("click",showromanizer);
 
-function berekeningromanizer(een, tien, honderd, duizend){
-    if(een == 4){
-        return "IV";
+function berekeningromanizer(getal){
+    let romanizer = "";
+    while(getal >= 0){
+        
     }
+    return romanizer;
 }
